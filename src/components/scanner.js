7dc6734cd43b870b8b7a5b1e0b0c38d1c8d27d41
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2023-11-08 20:32:35 +0530

    flashlight added

--- src/components/scanner.js
+++ src/components/scanner.js
@@ -1,37 +1,39 @@
 "use client";
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Html5QrcodeScanner } from 'html5-qrcode'
 
 
 const Scanner = () => {
 
-    const [Result,setResult]=useState()
+    const [Result, setResult] = useState()
 
-    useEffect(()=>{
-        const scanner = new Html5QrcodeScanner('reader',{
-            qrbox:{
-              width:250,
-              height:250
+    useEffect(() => {
+        const scanner = new Html5QrcodeScanner('reader', {
+            qrbox: {
+                width: 250,
+                height: 250
             },
-            fps:5,
-          })
-          scanner.render(success,error);
-          function success(result){
+            fps: 5,
+            showTorchButtonIfSupported: true
+        })
+        scanner.render(success, error);
+        function success(result) {
             scanner.clear();
             setResult(result)
-          }
-          function error(err) {
-        console.warn(err);
-          }
-    },[])
+        }
+        function error(err) {
+            console.warn(err);
+        }
+    }, [])
 
-  
-  return (
-    <div>
-      <h1>QR CODE SCAN WITH NEXT JS</h1>
-      {Result?<h2>ID : {Result} </h2>:<div id="reader"></div>}
-    </div>
-  );
+
+    return (
+        <div className="flex flex-col items-center">
+            <h1>QR CODE SCAN WITH NEXT JS</h1>
+           
+            {Result ? <h2>ID : {Result} </h2> : <div id="reader"></div>}
+        </div>
+    );
 };
 
 export default Scanner;
