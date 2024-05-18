0582186b3a36c75c99f5eae0ef47808464f84628
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2024-05-18 21:23:17 +0530

    updated

--- src/components/scanner.js
+++ src/components/scanner.js
@@ -1,38 +1,139 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Html5QrcodeScanner } from 'html5-qrcode'
-
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import * as XLSX from 'xlsx';
 
 const Scanner = () => {
 
-    const [Result, setResult] = useState()
+    const [selectedArea, setSelectedArea] = useState(11);
+    const [fixedNumbers, setFixedNumbers] = useState(null);
+    const [numbers, setNumbers] = useState([]);
+    const [count, setCount] = useState(1000);
 
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner('reader', {
-            qrbox: {
-                width: 250,
-                height: 250
-            },
-            fps: 5,
-            showTorchButtonIfSupported: true
-        })
-        scanner.render(success, error);
-        function success(result) {
-            scanner.clear();
-            setResult(result)
-        }
-        function error(err) {
-            console.warn(err);
-        }
+
     }, [])
 
+    const areaCodes = [
+        { district: 'Ampara', areacode: 63 },
+        { district: 'Anuradhapura', areacode: 25 },
+        { district: 'Avissawella', areacode: 36 },
+        { district: 'Badulla', areacode: 55 },
+        { district: 'Bandarawela', areacode: 57 },
+        { district: 'Batticaloa', areacode: 65 },
+        { district: 'Gampaha', areacode: 33 },
+        { district: 'Hambantota', areacode: 47 },
+        { district: 'Jaffna', areacode: 21 },
+        { district: 'Kalmunai', areacode: 67 },
+        { district: 'Kalutara', areacode: 34 },
+        { district: 'Kandy', areacode: 81 },
+        { district: 'Kegalle', areacode: 35 },
+        { district: 'Kurunegala', areacode: 37 },
+        { district: 'Mannar', areacode: 23 },
+        { district: 'Matale', areacode: 66 },
+        { district: 'Matara', areacode: 41 },
+        { district: 'Nawalapitiya', areacode: 54 },
+        { district: 'Negombo', areacode: 31 },
+        { district: 'Nuwara Eliya', areacode: 52 },
+        { district: 'Polonnaruwa', areacode: 27 },
+        { district: 'Puttalam', areacode: 32 },
+        { district: 'Ratnapura', areacode: 45 },
+        { district: 'Trincomalee', areacode: 26 },
+        { district: 'Vavuniya', areacode: 24 }
+    ];
+
+    const formSubmit = (e) => {
+        e.preventDefault()
+        const fixedNoLength= fixedNumbers==null?0:fixedNumbers.length;
+        generateNumbers((7-fixedNoLength), count);
+    }
+
+    const generateNumbers = (length, noCount) => {
+        const newNumbers = [];
+        for (let i = 0; i < noCount; i++) {
+            let randomNumber = Math.floor(Math.random() * 10 ** length) + 10 ** length; 
+
+            // Ensure unique numbers within the specified length
+            while (newNumbers.includes(randomNumber) && newNumbers.length == length) {
+                randomNumber = Math.floor(Math.random() * (10 ** length));
+            }
+            newNumbers.push({Numbers:`0${selectedArea}${fixedNumbers!=null?fixedNumbers:''}${randomNumber.toString()}`}); // Convert to string for display
+        }
+
+        const worksheet = XLSX.utils.json_to_sheet(newNumbers);
+        const workbook = XLSX.utils.book_new();
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(excelBlob);
+        link.download = 'numberList.xlsx';
+        link.click();
+    };
+
+
+    const handleSheetDownload = () => {
+        const json_data = [];
+
+        currentEvent.gusts.find((guest) => {
+            let obj = { Name: guest.name, Guest_ID: guest.guestId, Message: `Click here for RSVP (පැමිණීම තහවුරු කිරීම සදහා පිවිසෙන්න) 👉 www.evite.lk/guest?id=${guest.guestId}` };
+            json_data.push(obj);
+        })
+
+        const worksheet = XLSX.utils.json_to_sheet(json_data);
+        const workbook = XLSX.utils.book_new();
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(excelBlob);
+        link.download = 'my-data.xlsx';
+        link.click();
+
+    };
 
     return (
-        <div className="flex flex-col items-center">
-            <h1>QR CODE SCAN WITH NEXT JS</h1>
-           
-            {Result ? <h2>ID : {Result} </h2> : <div id="reader"></div>}
-        </div>
+        <Container>
+            <h1>Generate phone numbers </h1>
+            <Form onSubmit={formSubmit}>
+
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>districts</Form.Label>
+                        <Form.Select value={selectedArea} onChange={(e) => setSelectedArea(e.target.value)} defaultValue="Choose...">
+                            {areaCodes.map(area => (
+                                <option value={area.areacode}>{`${area.district} (0${area.areacode})`}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridCity">
+                        <Form.Label>Fixed numbers</Form.Label>
+                        <Form.Control placeholder="Eg:- 01122xxxxx" value={fixedNumbers} onChange={(e) => setFixedNumbers(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridZip">
+                        <Form.Label>Number count</Form.Label>
+                        <Form.Control value={count} onChange={(e) => setCount(e.target.value)} />
+                    </Form.Group>
+                </Row>
+
+                {/* <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+
+                <Button variant="primary" type="submit">
+                    Download Excel
+                </Button>
+            </Form>
+        </Container>
     );
 };
 
